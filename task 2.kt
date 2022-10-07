93a90a1ad31f8f69dd10131be7ad46a22199489d
dakeloon <94038117+dakeloon@users.noreply.github.com>
2022-10-07 13:53:02 +0700

    Create task 2.kt

---
+++ task 2.kt
@@ -0,0 +1,28 @@
+package com.example.logging
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
+import timber.log.Timber.Forest.plant
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        plant(Timber.DebugTree())
+    }
+
+    fun loggingWithLog(view: View) {
+        val logText = findViewById<EditText>(R.id.text).text.toString()
+        Timber.tag("From EditText").v(logText)
+    }
+
+    fun loggingWithTimber(view: View) {
+        val logText = findViewById<EditText>(R.id.text).text.toString()
+        Timber.d(logText)
+    }
+}
