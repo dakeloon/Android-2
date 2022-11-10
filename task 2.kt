85f32bf477f74bf555397a0caf9aca512d2714ec
dakeloon <94038117+dakeloon@users.noreply.github.com>
2022-11-10 13:17:04 +0700

    Update task 2.kt

--- task 2.kt
+++ task 2.kt
@@ -1,7 +1,8 @@
 package com.example.logging
 
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import timber.log.Timber
@@ -13,16 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val logbutton:Button = findViewById(R.id.button)
+        val timbutton:Button = findViewById(R.id.button2)
+
+
         plant(Timber.DebugTree())
-    }
 
-    fun loggingWithLog(view: View) {
-        val logText = findViewById<EditText>(R.id.text).text.toString()
-        Timber.tag("From EditText").v(logText)
-    }
+        logbutton.setOnClickListener {
+            val logText = findViewById<EditText>(R.id.text).text.toString()
+            Log.d("From EditText",logText)
+        }
 
-    fun loggingWithTimber(view: View) {
-        val logText = findViewById<EditText>(R.id.text).text.toString()
-        Timber.d(logText)
+        timbutton.setOnClickListener {
+            val logText = findViewById<EditText> (R.id.text).text.toString()
+            Timber.d(logText)
+        }
     }
 }
+
