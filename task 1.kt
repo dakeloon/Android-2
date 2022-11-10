616e92ec3282f2d3ce7251e1c4d1c0d5daf9b957
dakeloon <94038117+dakeloon@users.noreply.github.com>
2022-11-10 13:16:15 +0700

    Update task 1.kt

--- task 1.kt
+++ task 1.kt
@@ -1,10 +1,8 @@
-package com.example.toasthandler
+package com.example.toasthandler2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import com.example.toasthandler.R
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
